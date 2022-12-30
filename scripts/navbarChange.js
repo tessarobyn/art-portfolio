7ca030830f201735e5f380dd041b0d8fcfcc8a74
Tessa Grainger <tessa.robyn@outlook.com>
2022-12-30 16:38:12 +0000

    Changed navbar visibility on scroll

--- scripts/navbarChange.js
+++ scripts/navbarChange.js
@@ -1,8 +1,19 @@
 const ul = document.getElementById("navbarUl");
+const navbar = document.getElementById("navbar");
+let prevScrollPos = window.scrollY;
+let currentScrollPos;
 window.addEventListener("scroll", (event) => {
-  if (window.scrollY >= window.innerHeight) {
+  if (window.scrollY >= window.innerHeight - window.innerHeight / 3) {
     ul.style.backgroundColor = "#2e1760";
   } else {
     ul.style.backgroundColor = "rgba(0,0,0,0)";
   }
+
+  currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-" + navbar.offsetHeight + "px";
+  }
+  prevScrollPos = currentScrollPos;
 });
